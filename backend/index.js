594af5776215a6d45c73d3843c9354e693fb7856
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2024-01-02 20:13:01 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.post("/authenticate", async (req, res) => {
     const r  = await axios.put(
         'https://api.chatengine.io/users/',
         {username: username , secret: username, first_name:username},
-        {headers:{"private-key":"b88b1349-8ee5-4ca9-8cf6-ff0b0e2521d"}}
+        {headers:{"private-key":"b88b1349-8ee5-4ca9-8cf6-ff0b0e2521d4"}}
         )
         return res.status(r.status).json(r.data)
    
